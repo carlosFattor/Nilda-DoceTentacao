92ed7ad6f204fef73179b6c9097cef7a169200e6
Carlos Alexandre Fattor <carlos.fattor@gmail.com>
2015-04-04 15:15:04 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ play clean compile stage
 
 cd target/universal/stage
 sudo env PATH=$PATH play stop
-sudo -b env PATH=$PATH ./bin/lojinha -DapplyEvolutions.default=true
+sudo -b env PATH=$PATH ./bin/nilda-docetentacao -DapplyEvolutions.default=true
 cd ../../../
