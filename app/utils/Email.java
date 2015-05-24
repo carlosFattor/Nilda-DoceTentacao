41b50152ddea63651517fc6f489d201a5143599c
Carlos Alexandre Fattor <carlos.fattor@gmail.com>
2015-05-24 00:55:04 -0300

    Class Email

---
+++ app/utils/Email.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+	
+	private static Pattern pattern;
+	private static Matcher matcher;
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	public static boolean validateEmail(String email) {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+		matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+}
